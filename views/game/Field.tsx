f3c7ff268dda8133fbb8f79d6204d2927f2e8a88
Karolina Jeziorska <karolinajeziorska1@gmail.com>
2021-12-26 21:55:18 +0100

    Dodanie obrazków: strzałka, wyjście i ich wyświetlania

--- views/game/Field.tsx
+++ views/game/Field.tsx
@@ -1,9 +1,16 @@
 import React, { useContext } from 'react'
-import { Field } from '../../levels/fields'
+import { Field, Finish } from '../../levels/fields'
 import { DispatchContext } from './Game'
 import { CSSTransition } from 'react-transition-group'
 import { Directions } from '../../levels/level'
 import Dragon from './Dragon'
+
+import empty from '../../assets/images/empty.png'
+import hole from '../../assets/images/hole.png'
+import arrow from '../../assets/images/arrow.png'
+import rock from '../../assets/images/rock.png'
+import closed from '../../assets/images/closed.png'
+import scale from '../../assets/images/scale.png'
 
 interface FieldProps {
   field: Field;
@@ -40,10 +47,34 @@ export default function FieldComponent (props: FieldProps): React.ReactElement {
     dispatch({ type: 'FIELD_CLICK', payload: { index: props.field.id } })
   }
 
+  function renderImage () {
+    switch (props.field.typeOfField) {
+      case 'FINISH': {
+        const field = props.field as Finish
+        if (field.attributes.opened) return <img src={hole} alt="O" />
+        else return <img src={closed} alt="#" />
+      }
+      case 'ARROWRIGHT':
+        return <img src={arrow} alt="AR" />
+      case 'ARROWLEFT':
+        return <img style={{ transform: 'scaleX(-1)' }} src={arrow} alt="AL" />
+      case 'ARROWUP':
+        return <img style={{ transform: 'rotate(-90deg)' }} src={arrow} alt="AU" />
+      case 'ARROWDOWN':
+        return <img style={{ transform: 'rotate(90deg)' }} src={arrow} alt="AD" />
+      case 'WALL':
+        return <img src={rock} alt="W" />
+      case 'SCALE':
+        return <img src={scale} alt="AD" />
+      default:
+        return <img src={empty} alt="AR" />
+    }
+  }
+
   return (
     <>
       <div onClick={onClick} className='board-field'>
-        <div className="board-content">{props.field.image}</div>
+        <div className="board-content">{renderImage()}</div>
         <CSSTransition
           in={props.displayDragon}
           timeout={1000}
