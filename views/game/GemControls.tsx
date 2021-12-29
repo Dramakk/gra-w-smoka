dff060ea67941faa187746b77cb586556b1fd221
Karolina Jeziorska <karolinajeziorska1@gmail.com>
2021-12-29 20:13:32 +0100

    Dodanie obrazków kryształów

--- views/game/GemControls.tsx
+++ views/game/GemControls.tsx
@@ -3,6 +3,20 @@ import { GemColors } from '../../levels/level'
 import ValueWithControls from '../helpers/ValueWithControls'
 import { GemOwner } from './GemPanel'
 
+import blue from '../../assets/images/blue.png'
+import yellow from '../../assets/images/yellow.png'
+import red from '../../assets/images/red.png'
+import green from '../../assets/images/green.png'
+import black from '../../assets/images/black.png'
+
+export const crystalArray: Record<GemColors, any> = {
+  BLUE: blue,
+  YELLOW: yellow,
+  RED: red,
+  GREEN: green,
+  BLACK: black
+}
+
 export default function GemControls (props: {gemColor: GemColors, howMany: Record<GemOwner, number>, canEdit: boolean}): ReactElement {
   function buildItem (who: GemOwner): ReactElement {
     const addButtons = who === 'SCALE' ? false : props.canEdit
@@ -16,10 +30,9 @@ export default function GemControls (props: {gemColor: GemColors, howMany: Recor
         actionPayloadSubstract={{ who: who as 'TREE' | 'DRAGON', color: props.gemColor, changeInQty: -1 }}
       />
   }
-
   return (
     <div className="gem-panel-row">
-      <span>{props.gemColor}</span>
+      <img src={crystalArray[props.gemColor]} alt={props.gemColor}/>
       {Object.keys(props.howMany).map((key: GemOwner) => buildItem(key))}
     </div>
   )
