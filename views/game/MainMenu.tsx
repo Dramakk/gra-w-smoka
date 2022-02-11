cbbf6c9bab6f6c1251b27ea3f0bb05353e87c9b6
Dawid Żywczak <dawidkg9@wp.pl>
2022-02-11 18:20:08 +0100

    Fix paddingu

--- views/game/MainMenu.tsx
+++ views/game/MainMenu.tsx
@@ -30,26 +30,28 @@ export default function MainMenu (): React.ReactElement {
         Informacje o aplikacji
       </button>
       <SlideDown opened={infoDisplay}>
-        <div>
-          „Przygody smoka Bajtazara&quot; to ponowna implementacja „Gry w
-          Smoka&quot;. Pierwsza wersja aplikacji została zaprogramowana przez
-          Bartosza Doleckiego przy pomocy ActionScript i Adobe Flash Player.
-          Była ona dostępna nieodpłatnie na łamach Wrocławskiego Portalu
-          Informatycznego do czasu wyłączenia wsparcia dla wtyczki Adobe Flash
-          Player.
-        </div>
-        <br />
-        <div>
-          Celem gry jest otwarcie wyjścia oraz doprowadzenie do niego smoka. Po
-          ukończeniu poziomów dla początkujących, jedyną podpowiedzią dla gracza
-          jest nazwa poziomu oraz dostępne gadżety, co dodatkowo podnosi
-          trudność łamigłówek.
-        </div>
-        <br />
-        <div>
-          Ta wersja aplikacji napisana została przez studentów Wydziału
-          Matematyki i Informatyki Uniwersytetu Wrocławskiego, Karolinę
-          Jeziorską i Dawida Żywczaka, w ramach pracy dyplomowej.
+        <div className="game-description">
+          <div>
+            „Przygody smoka Bajtazara&quot; to ponowna implementacja „Gry w
+            Smoka&quot;. Pierwsza wersja aplikacji została zaprogramowana przez
+            Bartosza Doleckiego przy pomocy ActionScript i Adobe Flash Player.
+            Była ona dostępna nieodpłatnie na łamach Wrocławskiego Portalu
+            Informatycznego do czasu wyłączenia wsparcia dla wtyczki Adobe Flash
+            Player.
+          </div>
+          <br />
+          <div>
+            Celem gry jest otwarcie wyjścia oraz doprowadzenie do niego smoka.
+            Po ukończeniu poziomów dla początkujących, jedyną podpowiedzią dla
+            gracza jest nazwa poziomu oraz dostępne gadżety, co dodatkowo
+            podnosi trudność łamigłówek.
+          </div>
+          <br />
+          <div>
+            Ta wersja aplikacji napisana została przez studentów Wydziału
+            Matematyki i Informatyki Uniwersytetu Wrocławskiego, Karolinę
+            Jeziorską i Dawida Żywczaka, w ramach pracy dyplomowej.
+          </div>
         </div>
       </SlideDown>
     </div>
