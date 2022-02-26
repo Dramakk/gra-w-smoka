47d7207da10a79bcbc708d5ba87e4f471066cb28
Dawid Żywczak <dawidkg9@wp.pl>
2022-02-26 19:01:21 +0100

    Budowanie skryptu na produkcję

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,69 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "production",
+  devtool: "inline-source-map",
   entry: {
-    index: path.join(__dirname, 'index.tsx')
+    index: path.join(__dirname, "index.tsx"),
   },
-  target: 'web',
+  target: "web",
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: [".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: '/node_modules/'
+        use: "ts-loader",
+        exclude: "/node_modules/",
       },
       {
         test: /\.(scss|css)$/i,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-        exclude: '/node_modules/'
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+        exclude: "/node_modules/",
       },
       {
         test: /\.(png|jp(e*)g|svg|gif)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'images/'
-            }
-          }
-        ]
+              name: "[name].[ext]",
+              outputPath: "images/",
+            },
+          },
+        ],
       },
       {
         test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/'
-            }
-          }
-        ]
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
       },
-    ]
+    ],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'index.html')
+      template: path.join(__dirname, "index.html"),
     }),
     new CopyPlugin({
       patterns: [
-        {from: path.resolve(__dirname, 'levelConfigs')},
-        {from: path.resolve(__dirname, 'assets')}
+        { from: path.resolve(__dirname, "levelConfigs") },
+        { from: path.resolve(__dirname, "assets") },
       ],
     }),
-  ]
+  ],
 };
